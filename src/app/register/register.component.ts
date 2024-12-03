393270a4795b62080b45dd7b3e6a1183058e26bd
krasi-veleva <krasiveleva2002@gmail.com>
2024-12-03 23:54:32 +0200

    feat: add register and login components

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [RouterLink, FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {}
