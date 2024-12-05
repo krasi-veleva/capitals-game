37d95e064cba94e3d0b9afb728cb6f1af63c3deb
krasi-veleva <krasiveleva2002@gmail.com>
2024-12-06 00:02:38 +0200

    feat: update home component router navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToStatistic() {
+    this.router.navigate(['/statistic']);
+  }
 }
