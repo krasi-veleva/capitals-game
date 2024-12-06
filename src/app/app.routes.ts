a73d2bdfdda8917b345e07c40026fbd4811ee3dc
krasi-veleva <krasiveleva2002@gmail.com>
2024-12-06 17:00:53 +0200

    feat: add page not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { StatisticComponent } from './statistic/statistic.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'statistic', component: StatisticComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
