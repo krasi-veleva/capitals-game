8c34c406500a926ebf2ebf1dd3fd8b615b12062b
krasi-veleva <krasiveleva2002@gmail.com>
2024-12-11 16:03:41 +0200

    feat: add routing for edit component

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css',
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
   public imageURL = {
     image:
       'https://static.vecteezy.com/system/resources/previews/021/548/095/original/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg',
   };
+
+  public userId!: string;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    // this.route.params.subscribe((data) => {
+    //   console.log(data['id']);
+    // });
+
+    this.userId = this.route.snapshot.params['id'];
+    console.log(this.userId);
+  }
+
+  goToEdit() {
+    this.router.navigate(['edit-user', this.userId]);
+  }
 }
