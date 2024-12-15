16743f379083c0fbc0b72f2c7cf6199d3115e465
krasi-veleva <krasiveleva2002@gmail.com>
2024-12-16 00:15:19 +0200

    feat: add login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  errorMessage: string = '';
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  login() {
+  isEmailInvalid(form: NgForm): boolean {
+    const emailControl = form.controls['email'];
+
+    const regex =
+      /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    return !!(
+      emailControl?.invalid &&
+      (emailControl?.touched || emailControl?.dirty)
+    );
+  }
+
+  isPasswordInvalid(form: NgForm): boolean {
+    const passwordControl = form.controls['password'];
+    return !!(
+      passwordControl?.invalid &&
+      (passwordControl?.touched || passwordControl?.dirty)
+    );
+  }
+
+  login(form: NgForm) {
     this.authService
       .signIn(this.email, this.password)
       .then(() => {
@@ -24,6 +46,7 @@ export class LoginComponent {
         this.router.navigate(['/']);
       })
       .catch((error) => {
+        this.errorMessage = error.message;
         console.error('Error during login:', error);
       });
   }
